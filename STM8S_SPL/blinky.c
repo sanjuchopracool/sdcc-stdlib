80cbda52edec4342a0f8c41cc7b4f271e3beae7b
Sanjay Chopra <sanjuchopracool@gmail.com>
2017-10-09 22:55:13 +0530

    SPI is now working fine :P

--- STM8S_SPL/blinky.c
+++ STM8S_SPL/blinky.c
@@ -307,19 +307,69 @@ uint16_t getADCValue(uint8_t channel)
     ADC1_CR1 |= ADC1_CR1_ADON; // start conversion
 
     while(! (ADC1_CSR & ADC1_FLAG_EOC) );
-//    // MSB at High and LSB at LOW
+    //    // MSB at High and LSB at LOW
     value = (uint16_t)(ADC1_DRL | (uint16_t)ADC1_DRH << 2);
     ADC1_CSR &= (~ADC1_FLAG_EOC);
     return value;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+/////////////////////////// SPI ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+#define SPI_CR1         *(__IO uint8_t*)0x5200
+#define SPI_CR2         *(__IO uint8_t*)0x5201
+#define SPI_ICR         *(__IO uint8_t*)0x5202
+#define SPI_SR         *(__IO uint8_t*)0x5203
+#define SPI_DR         *(__IO uint8_t*)0x5204
 
+#define SD_SPI_SCK_PIN                   GPIO_PIN_5                  /* PC.05 */
+#define SD_SPI_SCK_GPIO_PORT             GPIOC                       /* GPIOC */
+#define SD_SPI_MISO_PIN                  GPIO_PIN_7                  /* PC.05 */
+#define SD_SPI_MISO_GPIO_PORT            GPIOC                       /* GPIOC */
+#define SD_SPI_MOSI_PIN                  GPIO_PIN_6                  /* PC.06 */
+#define SD_SPI_MOSI_GPIO_PORT            GPIOC                       /* GPIOC */
+#define SD_CS_PIN                        GPIO_PIN_3                  /* PE.03 */
+#define SD_CS_GPIO_PORT                  GPIOA                       /* GPIOA */
+
+#define CS_LOW()     PA_ODR &= (~(0x08))
+#define CS_HIGH()    PA_ODR |= 0x08;
+///////////////////////////////////////////////////////////////////////////////
+void initSPI()
+{
+    // By default the clock is enabled
+    // No change required for mosi, miso, sck
+
+    SPI_CR1 = 0x44;
+    SPI_CR2 = 0x03;
+
+    // Set CS pin in Output push-pull high level.
+    PA_DDR |= 0x08;
+    PA_CR1 |= 0x08;
+    PA_CR2 |= 0x08;
+}
+
+uint8_t writeRead(uint8_t inData)
+{
+    SPI_DR = inData;
+    while(!(SPI_SR & 0x01 ));
+    return (SPI_DR);
+}
+uint8_t getRegister(uint8_t inReg)
+{
+    uint8_t data = 0;
+    CS_LOW();
+    writeRead(inReg);
+    data = writeRead(0xFF);
+    CS_HIGH();
+    return data;
+}
+
+///////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    uint16_t adcValue2 = 0;
-    uint16_t adcValue3 = 0;
-    uint16_t adcValue4 = 0;
+    uint8_t value = 0;
+    uint8_t index = 0;
+
     setUpClock();
     setUpSerial();
     // Configure pins
@@ -335,15 +385,17 @@ int main()
     // Fmster/4, single conversion mode, enable ADC
     ADC1_CR1 = 0x21;
 
+    initSPI();
     // keep default alignment, no scan node, no trigger
     while( 1 )
     {
-        adcValue2 = getADCValue(2);
-        adcValue3 = getADCValue(3);
-        adcValue4 = getADCValue(4);
-        PB_ODR ^= 0x20;
-        printf("%d  %d  %d\n", adcValue2, adcValue3, adcValue4);
-        delay( 100 );
+        printf("Working?\n");
+        for( index = 0; index < 8; ++index)
+        {
+            value = getRegister(index);
+            printf("%d %d\n",index, value);
+        }
+        delay( 1000 );
     }
 }
 
--- STM8S_SPL/stm8s_conf.h
+++ STM8S_SPL/stm8s_conf.h
@@ -55,7 +55,7 @@
 //#include "stm8s_itc.h"
 //#include "stm8s_iwdg.h"
 //#include "stm8s_rst.h"
-//#include "stm8s_spi.h"
+#include "stm8s_spi.h"
 //#include "stm8s_tim1.h"
 #ifndef STM8S903
 // #include "stm8s_tim2.h"
